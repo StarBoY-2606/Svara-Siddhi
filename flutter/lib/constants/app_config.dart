825be4609813dd749838d192b06744dede6fea59
Kartik Maihuria <kartik_m@ece.iitr.ac.in>
2026-05-21 22:57:47 +0530

    Connect app to live backend and fix build workflow syntax

--- flutter/lib/constants/app_config.dart
+++ flutter/lib/constants/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   
-  static const String apiBaseUrl = 'http://172.26.104.51:8000';
+  static const String apiBaseUrl = 'https://svara-siddhi.onrender.com';
 
   static const int maxRecordingSeconds = 30;
   static const int minRecordingSeconds = 10;
